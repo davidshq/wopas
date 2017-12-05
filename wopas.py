6809b85d73f78125aff462cdc41d58e171ab80ae
Dave Mackey <dave@davemackey.net>
2017-12-04 22:11:04 -0500

    - Added ability to choose how many plugins you want to be returned per response. - Theoretically added ability to choose how plugins are sorted, but due to inconsistency in WP.org API, this feature is not curently functional. - Removed TODO that was completed.

--- wopas.py
+++ wopas.py
@@ -17,8 +17,16 @@ def get_plugin_info_page(page_number):
     # Added the per_page param to the endpoint so we can get more results per request
     # Each request will also take longer, so we're slowing down our rate a bit
     # TODO: Take user input for requests per page, type of browse, page range (including all)
+    plugins_per_page = input('How many plugins do you want to be returned in each API response? [Default: 250]')
+    plugins_order = input('In what order do you want the results returned? [Default: popular]')
+    if not plugins_per_page:
+        plugins_per_page = 250
+    # Bahh! WP.org API is returning a dict when querying popular but a list when querying featured
+    # For right now, just use featured, until I work out logic to respond with processing by dict/list as needed.
+    if not plugins_order:
+        plugins_order = 'popular'
     url = 'https://api.wordpress.org/plugins/info/1.1/?action=query_plugins& \
-           request[per_page]=250&request[browse]=popular&request[page]={}'.format(page_number)
+           request[per_page]={}&request[browse]={}&request[page]={}'.format(plugins_per_page, plugins_order, page_number)
     response = requests.get(url)
     json_data = response.json()
 
@@ -53,7 +61,6 @@ while page_number is not last_page + 1:
         # Save to a Folder so we don't clutter current directory
         file = "plugin-{}.json".format(plugin['slug'])  # Slug is the plugin name without spaces, usually used to create URLs
 
-        # TODO: Remember to make 'plugins' a variable once user input accepted.
         with open(os.path.join(selectpath, file), 'w+', encoding='utf-8', errors="replace") as outfile:
             json_data = json.dumps(plugin, sort_keys=False, indent=4) # TODO: Add user input parameters for options.
             outfile.write(json_data)
