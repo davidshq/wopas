f8319e93bdde3764dd9156d8ead2b833deb12575
Dave Mackey <dave@davemackey.net>
2018-05-14 17:36:55 -0400

    - Currently hard coded remove of description and sections from JSON response from API. Significantly reduces length of each plugin listing. - WP.org API inconsistently returns dicts or lists, added code that detects whether the returned is dict or list and then converts lists to dict in python. - Fixed logic relating to number of plugins to be returned per page, was receiving as string, now integer. - Added a plugin_number variable that allows for numbering each plugin in numeric order and is used to determine when to close the array containing all JSON plugin objects. - Corrected code that was using is and should have been using ==. - Added logic to add [ and ] at begining and end of single file JSON, if opted for. - Added logic to place commas between JSON objects if in single file. - Huge HT to AlexCoventry, Rascal_Two, and Zigity_Zagity over on Reddit LearnPython for helping me work through this logic.

--- get_plugin_info_page.py
+++ get_plugin_info_page.py
@@ -12,11 +12,18 @@ def get_plugin_info_page(page_number, plugins_per_page, plugins_order, multiple_
     # Added the per_page param to the endpoint so we can get more results per request
     # Each request will also take longer, so we're slowing down our rate a bit
     url = 'https://api.wordpress.org/plugins/info/1.1/?action=query_plugins& \
-           request[per_page]={}&request[browse]={}&request[page]={}'.format(plugins_per_page, plugins_order, page_number)
+           request[per_page]={}&request[browse]={}&request[page]={}&request[fields][description]=0&request[fields][sections]=0'.format(plugins_per_page, plugins_order, page_number)
     response = requests.get(url)
     json_data = response.json()
 
     num_pages = json_data['info']['pages']
-    plugins = json_data['plugins']
+    raw_plugins = json_data['plugins']
+    plugins = raw_plugins
 
-    return plugins, num_pages
+    if type(plugins) == dict:
+        return plugins, num_pages
+    else:
+        plugins = {}
+        for i, plugin in enumerate(raw_plugins):
+            plugins[i] = plugin
+        return plugins, num_pages
--- wopas.py
+++ wopas.py
@@ -22,6 +22,8 @@ plugins_order = input('In what order do you want the results returned? [Default:
 multiple_files = input('Should each plugin be saved to a separate JSON file? [Default: N]')
 if not plugins_per_page:
     plugins_per_page = 250
+else:
+    plugins_per_page = int(plugins_per_page)
 # Bahh! WP.org API is returning a dict when querying popular but a list when querying featured
 # For right now, just use popular, until I work out logic to respond with processing by dict/list as needed.
 if not plugins_order:
@@ -33,6 +35,7 @@ if not multiple_files:
 # TODO: This will need to change once user input is accepted
 last_page = 1
 page_number = 1
+plugin_number = 1
 
 # Loops until page number = last_page (+1 because we still want the last page)
 while page_number is not last_page + 1:
@@ -46,18 +49,28 @@ while page_number is not last_page + 1:
     for pid, plugin in plugins.items():
         plugin_name = plugin['name']
 
-        if multiple_files is 'N':
+        if multiple_files == 'N':
             file = "wp-plugins.json"
 
-        if multiple_files is 'Y':
+        if multiple_files == 'Y':
             file = "plugin-{}.json".format(plugin['slug'])  # Slug is the plugin name without spaces, usually used to create URLs
 
         with open(os.path.join(selectpath, file), 'a+', encoding='utf-8', errors="replace") as outfile:
+            if plugin_number == 1 and multiple_files == 'N':
+                outfile.write('[')
+
             json_data = json.dumps(plugin, sort_keys=False, indent=4) # TODO: Add user input parameters for options.
             outfile.write(json_data)
-            print("Plugin {} added".format(plugin_name))
+            if multiple_files == 'N':
+                outfile.write(',')
+            print("Plugin Number {} Named: {} added".format(plugin_number, plugin_name))
+        plugin_number +=1
 
     print("Page {} of {} total pages complete".format(page_number, last_page))
     page_number += 1
     # TODO: Maybe include time.sleep as user input parameter?
     time.sleep(2)
+
+if multiple_files == 'N':
+    with open(os.path.join(selectpath, file), 'a+', encoding='utf-8', errors="replace") as outfile:
+        outfile.write(']')
